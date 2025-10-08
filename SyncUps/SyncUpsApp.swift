7f0db83db910bf8f8f9a81980a427610f649d6fc
imsyf <29587914+imsyf@users.noreply.github.com>
2025-10-08 19:59:52 +0700

    Initial commit

---
+++ SyncUps/SyncUpsApp.swift
@@ -0,0 +1,16 @@
+//
+//  SyncUpsApp.swift
+//  SyncUps
+//
+//  Created by Imam on 08/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SyncUpsApp: App {
+    var body: some Scene {
+        WindowGroup {
+        }
+    }
+}
---
+++ SyncUpsTests/SyncUpsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SyncUpsTests.swift
+//  SyncUpsTests
+//
+//  Created by Imam on 08/10/25.
+//
+
+import Testing
+@testable import SyncUps
+
+struct SyncUpsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
