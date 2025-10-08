3a516d9c5ff5490384c3f633faf4e695dccec125
imsyf <29587914+imsyf@users.noreply.github.com>
2025-10-08 21:04:17 +0700

    Display list of sync-ups

--- SyncUps/SyncUpsApp.swift
+++ SyncUps/SyncUpsApp.swift
@@ -5,12 +5,19 @@
 //  Created by Imam on 08/10/25.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct SyncUpsApp: App {
+    static let store = Store(initialState: SyncUpsList.State(syncUps: [.mock])) {
+        SyncUpsList()
+            ._printChanges()
+    }
+    
     var body: some Scene {
         WindowGroup {
+            SyncUpsListView(store: Self.store)
         }
     }
 }
